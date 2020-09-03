806e17306049bac328a27708b986ae65b73169aa
chienleow <chienleow1@gmail.com>
2020-09-02 22:46:41 -0400

    Add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   has_many :goals, through: :user_goals
   belongs_to :team
 
+  validates [:first_name, :last_name], presence: true
+  validates :username, uniqueness: true, presence: true
+
   has_secure_password
 end
