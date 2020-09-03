33db462a38d616fe79df5455f7f4b1c8153617be
chienleow <chienleow1@gmail.com>
2020-09-03 15:53:24 -0400

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-team_a = Team.create(team_name: "Team A", team_points: "0")
-team_b = Team.create(team_name: "Team B", team_points: "0")
-team_c = Team.create(team_name: "Team C", team_points: "0")
+team_a = Team.create(team_name: "Team A")
+team_b = Team.create(team_name: "Team B")
+team_c = Team.create(team_name: "Team C")
