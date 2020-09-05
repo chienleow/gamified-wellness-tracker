e21773b92946ea44b90ed25fab92ffdcd5933036
chienleow <chienleow1@gmail.com>
2020-09-04 22:20:11 -0400

    Add create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,16 @@ class SessionsController < ApplicationController
         reset_session
         redirect_to '/'
     end
+
+    def create
+        # byebug
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:message] = "Login Error, please try again."
+            redirect_to login_path
+        end
+    end
 end
