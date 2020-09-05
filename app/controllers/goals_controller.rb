57a3a6263ec910d6cfe69d5c07a7cd02c0da0e85
chienleow <chienleow1@gmail.com>
2020-09-05 16:54:27 -0400

    Seed Goal data

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,3 +1,25 @@
 class GoalsController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def new
+        @goal = Goal.new
+    end
+
+    def create
+        @goal = Goal.new(goal_params)
+    end
+
+    def show
+        @goal = Goal.find_by_id(params[:id])
+    end
+
+    def index
+        @goals = Goal.all
+    end
+
+    private
+
+        def goal_params
+            params.require(:goal).permit(:category, :description)
+        end
 end
