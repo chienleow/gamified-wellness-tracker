808af1aadcbd1337ac1ea8da50344a5aeb31eb79
chienleow <chienleow1@gmail.com>
2020-09-05 16:33:09 -0400

    Seeds to Goal

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Seeds for Team
 team_a = Team.create(team_name: "Team A")
 team_b = Team.create(team_name: "Team B")
 team_c = Team.create(team_name: "Team C")
+
+# Seeds for Goal
+physical_1 = Team.create(category: "Physical", description: "6 hours of sleep")
+physical_2 = Team.create(category: "Physical", description: "Exercise")
+mental_1 = Team.create(category: "Mental", description: "Thankful list")
+mental_2 = Team.create(category: "Mental", description: "Journal/Reflection")
+intellectual_1 = Team.create(category: "Intellectual", description: "Podcast")
+intellectual_2 = Team.create(category: "Intellectual", description: "Book/Article")
+daily_1 = Team.create(category: "Daily Top 5", description: "Thing 1")
+daily_2 = Team.create(category: "Daily Top 5", description: "Thing 2")
+daily_3 = Team.create(category: "Daily Top 5", description: "Thing 3")
+daily_4 = Team.create(category: "Daily Top 5", description: "Thing 4")
+daily_5 = Team.create(category: "Daily Top 5", description: "Thing 5")
+
