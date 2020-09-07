e8dae49cc2915b134993c0ab81ae4412aeb2c992
chienleow <chienleow1@gmail.com>
2020-09-07 15:24:05 -0400

    Add usergoal create action

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -2,10 +2,20 @@ class UserGoalsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
     def create
-        @usergoal = UserGoal.new
+        @usergoal = UserGoal.new(usergoal_params)
         @usergoal.save
         # set user.id to usergoal.user_id
         # set goal.id to usergoal.goal_id
-        redirect_to goals_path
+        redirect_to usergoal_path(@usergoal)
     end
+
+    def index
+        @usergoals = UserGoal.all
+    end
+    
+    private
+
+        def usergoal_params
+            params.require(:usergoal).permit(:user_id, :goal_id, :complete, :notes)
+        end
 end
