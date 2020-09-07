50c3ea6e6934db0cacc6336032c8b638b19585ec
chienleow <chienleow1@gmail.com>
2020-09-06 23:46:33 -0400

    Update UserGoal Controller

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -1,3 +1,11 @@
 class UserGoalsController < ApplicationController
     before_action :redirect_if_not_logged_in
+
+    def create
+        @usergoal = UserGoal.new
+        @usergoal.save
+        # set user.id to usergoal.user_id
+        # set goal.id to usergoal.goal_id
+        redirect_to goals_path
+    end
 end
