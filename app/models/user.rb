84c4267617b5dd7cb32781381140cb02886c467e
chienleow <chienleow1@gmail.com>
2020-09-10 19:43:39 -0400

    Validate user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :goals, through: :user_goals
   belongs_to :team
 
-  # validates [:first_name, :last_name], presence: true
-  validates :username, uniqueness: true, presence: true
+  validates :first_name, :last_name, :username, :presence => true
+  validates :email, uniqueness: true, presence: true
 
   has_secure_password
 end
