38ef83e8224973d50c8541e8e50aa3cd1f24c02a
chienleow <chienleow1@gmail.com>
2020-09-12 19:32:10 -0400

    Automate login with google without redirecting to edit page if google user has already exist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -30,23 +30,29 @@ class SessionsController < ApplicationController
     end
 
     def google
-        @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
-            user.username = auth[:info][:first_name]
-            user.email = auth[:info][:email]
-            user.password = SecureRandom.hex(10)
-            user.team = Team.all.first #sets a random team for the user first, have user change later
-        end
-        # set a fake team first (so I don't have to carry @user)
-        # render a page for google users to choose a team
-        # update method for changing fake team to user desired team
-        if @user.save
+        oauth_email = auth["info"]["email"]
+        if @user = User.find_by(email: oauth_email)
             session[:user_id] = @user.id
-            # render :team_select_page
-            # update method for changing fake team to user desired team
-            redirect_to edit_user_path(@user)
-            # redirect_to user_path(@user)
+            redirect_to user_path(@user)
         else
-            redirect_to '/'
+            @user = User.create(email: oauth_email) do |user|
+                user.username = auth[:info][:first_name]
+                user.email = auth[:info][:email]
+                user.password = SecureRandom.hex(10)
+                user.team = Team.all.first #sets a random team for the user first, have user change later
+            end
+            # set a fake team first (so I don't have to carry @user)
+            # render a page for google users to choose a team
+            # update method for changing fake team to user desired team
+            if @user.save
+                session[:user_id] = @user.id
+                # render :team_select_page
+                # update method for changing fake team to user desired team
+                redirect_to edit_user_path(@user)
+                # redirect_to user_path(@user)
+            else
+                redirect_to '/'
+            end
         end
     end
 
